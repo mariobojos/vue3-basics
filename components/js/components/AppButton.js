6ec9b93c717897e96aac4effe93faf8dc2069108
Mario Bojos <m4riobojosd@gmail.com>
2023-11-26 13:10:08 +0800

    06 - Type prop affects button class binding

--- components/js/components/AppButton.js
+++ components/js/components/AppButton.js
@@ -1,8 +1,20 @@
 export default {
 
+    /*
+    NOTE:
+    :class binding can hold a String, Object
+    In the example below, we are using an Object with the CSS classes as the name of the Object attribute with
+        boolean value.
+     */
     template: `
         <button
-            class="bg-green-800 text-yellow-100 hover:bg-green-600 disabled:bg-gray-400 rounded py-3 px-3"
+            :class="{
+            'hover:bg-green-600 disabled:cursor-not-allowed disabled:bg-gray-400 rounded py-3 px-3': true,
+            'bg-blue-800 text-blue-100': type === 'primary',
+            'bg-pink-800 text-pink-100': type === 'secondary',
+            'bg-amber-800 text-amber-100': type === 'warning',
+            'bg-gray-800 text-gray-100': type === 'muted',
+            }"
             :disabled="processing"
         >
             <slot />
@@ -10,12 +22,15 @@ export default {
         `,
 
     props: {
-        type: String,
+        type: {
+            type: String,
+            default: 'primary'
+        }
     },
 
     data() {
         return {
-            processing: true
+            processing: false
         }
     },
 
