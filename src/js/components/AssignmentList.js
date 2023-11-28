c0e13e4feb8bbfc5a43885786ff74d05607e5e2e
Mario Bojos <m4riobojosd@gmail.com>
2023-11-28 12:16:24 +0800

    08 - Add form with event handling

--- src/js/components/AssignmentList.js
+++ src/js/components/AssignmentList.js
@@ -10,7 +10,7 @@ export default {
          <section v-show="assignments.length">
             <h2 class="font-bold my-2">{{ title }}</h2>
     
-            <ul class="border border-gray-700 rounded divide-y divide-gray-700">
+            <ul class="border border-gray-400 rounded divide-y divide-gray-400">
                 <assignment 
                     v-for="assignment in assignments" 
                     :key="assignment.id" 
--- src/js/components/Assignments.js
+++ src/js/components/Assignments.js
@@ -10,7 +10,15 @@ export default {
         <section class="space-y-7">
             <assignment-list :assignments="filters.inProgress" title="In Progress Assignments" />
             <assignment-list :assignments="filters.completed" title="Completed Assignments" />
+            
+            <form @submit.prevent="add">
+                <div class="border border-gray-500 rounded text-black">
+                    <input v-model="newAssignment" type="text" class="bg-white p-2" placeholder="New assignment..." />
+                    <button type="submit" class="bg-white p-2 border-l">Add</button>
+                </div>
+            </form>
         </section>
+        
     `,
 
     data() {
@@ -20,7 +28,8 @@ export default {
                 { name: 'Take a bath', done: false, id: 2 },
                 { name: 'Eat breakfast', done: false, id: 3 },
                 { name: 'Brush teeth', done: false, id: 4 },
-            ]
+            ],
+            newAssignment: '',
         }
     },
 
@@ -31,5 +40,18 @@ export default {
                 completed: this.assignments.filter(assignment => assignment.done),
             }
         }
+    },
+
+    methods: {
+        add() {
+            this.assignments.push({
+               name: this.newAssignment,
+               done: false,
+               id: this.assignments.length + 1
+            });
+
+            this.newAssignment = '';
+        },
     }
+
 }
